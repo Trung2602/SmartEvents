f9ae89636d38f91fd733cde90b4fb9c3163538d7
admin <luhieutrung2602@gmail.com>
2025-11-28 12:41:39 +0700

    trung: 28/11

--- events/src/main/java/com/aws/services/ChannelFollowerService.java
+++ events/src/main/java/com/aws/services/ChannelFollowerService.java
@@ -1,6 +1,7 @@
 package com.aws.services;
 
 import com.aws.pojo.ChannelFollower;
+import org.springframework.data.domain.Page;
 
 import java.util.UUID;
 
@@ -8,4 +9,6 @@ public interface ChannelFollowerService {
     ChannelFollower findById(UUID uuid);
     ChannelFollower addOrUpdateChannelFollower(ChannelFollower channelFollower);
     void deleteChannelFollower(ChannelFollower channelFollower);
+    public Page<ChannelFollower> getChannelFollowersByChannelUuid(UUID channelUuid, int page, int size);
+    public Page<ChannelFollower> getChannelFollowersByFollowerUuid(UUID followerlUuid, int page, int size);;
 }
--- events/src/main/java/com/aws/services/impl/ChannelMemberServiceImpl.java
+++ events/src/main/java/com/aws/services/impl/ChannelMemberServiceImpl.java
@@ -1,9 +1,13 @@
 package com.aws.services.impl;
 
+import com.aws.pojo.ChannelFollower;
 import com.aws.pojo.ChannelMember;
 import com.aws.repositories.ChannelMemberRepository;
 import com.aws.services.ChannelMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -29,5 +33,23 @@ public class ChannelMemberServiceImpl implements ChannelMemberService {
     public ChannelMember findById(UUID uuid) {
         Optional<ChannelMember> channelMember = this.channelMemberRepository.findById(uuid);
         return channelMember.orElse(null);
+    }
+
+    @Override
+    public Page<ChannelMember> getChannelMembersByChannelUuid(UUID channelUuid, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return channelMemberRepository.findByChannel_Uuid(channelUuid, pageable);
+    }
+
+    @Override
+    public Page<ChannelMember> getChannelMembersByUserUuid(UUID userUuid, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return channelMemberRepository.findByUsers_Uuid(userUuid, pageable);
+    }
+
+    @Override
+    public Page<ChannelMember> getChannelMembersByInvitationStatus(String invitationStatus, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return channelMemberRepository.findByInvitationStatus(invitationStatus, pageable);
     }
 }
